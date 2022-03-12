017ff001bb75d9fc7eac3788bf6454171bf8d5b1
hugosenari <hugosenari@gmail.com>
2022-03-12 01:45:38 -0300

    chore: re index unpack

---
+++ master/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''Array/Sequence/Object destructuring/unpacking macro'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-unpack-master.flake = false;
+  inputs.src-unpack-master.owner = "technicallyagd";
+  inputs.src-unpack-master.ref   = "refs/heads/master";
+  inputs.src-unpack-master.repo  = "unpack";
+  inputs.src-unpack-master.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-unpack-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-unpack-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
